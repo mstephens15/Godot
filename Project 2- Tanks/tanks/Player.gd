0bdef5de9ac164a75f0039f03572c6ff55afbab5
Mitch Stephens <mstephens@sandiego.edu>
2020-07-11 06:41:59 -0700

    Tank can now be moved

---
+++ Project 2- Tanks/tanks/Player.gd
@@ -0,0 +1,19 @@
+extends "res://tanks/tank.gd"
+
+# override control function for our specific character
+
+func control(delta):
+	$Turret.look_at(get_global_mouse_position())
+	var rot_dir = 0
+	if Input.is_action_pressed("turn_right"):
+		rot_dir += 1
+	if Input.is_action_pressed("turn_left"):
+		rot_dir -= 1
+	rotation += rotation_speed * rot_dir * delta
+	velocity = Vector2()
+	if Input.is_action_pressed("forward"):
+		velocity = Vector2(speed, 0).rotated(rotation)
+	if Input.is_action_pressed("back"):
+		velocity = Vector2(-speed/2, 0).rotated(rotation)
+		
+
--- Project 2- Tanks/tanks/tank.gd
+++ Project 2- Tanks/tanks/tank.gd
@@ -1,16 +1,33 @@
+# our generic tank script, used as a baseline for player and enemy tanks
 extends KinematicBody2D
 
+signal health_changed
+signal dead             # when tank dies
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (PackedScene) var Bullet
+export (int) var speed             # how fast bullet moves
+export (float) var rotation_speed  # how fast tank rotates
+export (float) var gun_cooldown
+export (int) var health
 
+var velocity = Vector2()
+var can_shoot = true
+var alive = true
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$GunTimer.wait_time = gun_cooldown
+	
+func control(delta):    # called every frame, let you input keyboard controls 
+	pass
+	
+func _physics_process(delta):
+	if not alive:
+		return
+	control(delta)
+	move_and_slide(velocity)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# quit if hitting escape
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			get_tree().quit()

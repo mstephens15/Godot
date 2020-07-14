e4214b119e55530237bb07f1d44259b2cde13b3b
Mitch Stephens <mstephens@sandiego.edu>
2020-07-14 15:57:21 -0700

    Added enemy tank path

---
+++ Project 2- Tanks/tanks/EnemyTank.gd
@@ -0,0 +1,35 @@
+extends "res://tanks/tank.gd"
+
+onready var parent = get_parent()
+
+export (float) var turret_speed
+export (int) var detect_radius
+
+var target = null
+
+func _ready():
+	$DetectRadius/CollisionShape2D.shape.radius = detect_radius
+
+func control(delta):
+	if parent is PathFollow2D:
+		parent.set_offset(parent.get_offset() + speed * delta)
+		position = Vector2()       # will always keep enemy tank on path; had an issue bc enemy and us both kinematic so it would get stuck; this is relative to parent position
+	else:
+		#other movement
+		pass
+
+# linear_interpolate transforms one value to another at constant speed, aka turret_speed * delta
+# basically, it is rotating from the current_dir to the target_dir at that current speed
+func _process(delta):
+	if target:
+		var target_dir = (target.global_position - global_position).normalized()   # where the target (player) is in the world
+		var current_dir = Vector2(1, 0).rotated($Turret.global_rotation)           # how the enemy tank is currently rotated
+		$Turret.global_rotation = current_dir.linear_interpolate(target_dir, turret_speed*delta).angle()
+
+func _on_DetectRadius_body_entered(body):
+	if body.name == "Player":
+		target = body
+
+func _on_DetectRadius_body_exited(body):
+	if body == target:
+		target = null       # drop target if target leaves area

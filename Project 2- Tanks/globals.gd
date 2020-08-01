9adcaa463cba05b4892ab1da9a3364047446528e
Mitch Stephens <mstephens@sandiego.edu>
2020-08-01 09:33:54 -0700

    Added ability to switch between title and level

--- Project 2- Tanks/globals.gd
+++ Project 2- Tanks/globals.gd
@@ -4,3 +4,16 @@ extends Node
 # because of that, every scene will now automatically load this 
 
 var slow_terrain = [0, 10, 20, 30, 7, 8, 17, 18]
+var current_level = 0    # what level we are on in the game
+var levels = ["res://ui/TitleScreen.tscn", "res://maps/Map01.tscn"]
+
+func restart():
+	current_level = 0
+	get_tree().change_scene(levels[current_level])
+	
+func next_level():
+	current_level += 1
+	if current_level < levels.size():
+		get_tree().change_scene(levels[current_level])
+	else:
+		restart()
